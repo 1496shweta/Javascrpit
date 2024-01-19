75a53edf84d3a179b290ac45f146ef819f50b16f
1496shweta <124311422+1496shweta@users.noreply.github.com>
2024-01-19 12:08:54 +0000

    container config done

---
+++ Javascript/let_var_const.js
@@ -0,0 +1,8 @@
+const account_Id=45628
+let account_Name= "Shweta"
+var account_Email= "shwetasharmaa8020@google.com"
+account_Id=22222
+console.log(account_Id);
+console.log(account_Name);
+account_Email="ggdgggd"
+console.log(account_Email);
