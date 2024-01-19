ac455866db77a494096afcf57b399129a962e7aa
1496shweta <124311422+1496shweta@users.noreply.github.com>
2024-01-19 13:46:48 +0000

    string

---
+++ Javascript/string_to_number.js
@@ -0,0 +1,49 @@
+let value=3
+let negValue= -value 
+//console.log(negValue)
+
+/* console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**5)
+console.log(2/3)
+console.log(2%3)*/
+
+
+let str1="shweta"
+let str2="Krishna"
+console.log(str1+str2)
+
+// console.log("1" +2)
+// console.log("1"+"2")
+// console.log(1+ 1+"2")
+// console.log("1"+2+2)
+// console.log(3+4/95% 3)
+// console.log(+true);
+// let num1,+"num2,num3
+// num1=num2=num3=4 
+// num1++
+// console.log(num1)
+// console.log(++num2)
+// console.log(num3++)
+
+
+//comparsion
+// console.log("2">1)
+// console.log("2">"1")
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null==NaN)
+console.log(true +false)
+console.log(false+ true)
+console.log(false-true)
+console.log(9-"5")
+console.log(""+ "")
+console.log(""-"")
+
+// The reason is that an equality check == and comparsion >< >= <= works differently 
+// comaparsion convert null to a number , treating it as 0,
+// That's why (3) null>=0 is true and (1) null> is true
+
+console.log("2"=== 2)
