8d910269e0928a825131f440d00a011484703668
1496shweta <124311422+1496shweta@users.noreply.github.com>
2024-01-19 14:02:49 +0000

    0

---
+++ Javascript/DataTypeS.js
@@ -0,0 +1,6 @@
+// Primitive Data Type - Reference type - call by value - Inkko jb referenvce kta hj atah to data k value ki copy provide hoti h na ki memeory allocation 
+//7 type :String , Number , Boolean , null, undefined, Symbol, bigInit 
+// Reference (Non Primitive)
+// Array , Object , Function 
+// 
+// Varible ka type kya ha- JavaScript is a dynamically typed language. This means that the type of a variable is determined at runtime, and you can change the type of a variable during the execution of a program. In contrast, statically typed languages determine variable types at compile-time, and the type of a variable is fixed once it is declared.
